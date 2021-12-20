d314dbfe553cb66977eef42bcbbd1353f2f92f56
Yuri Ponomarev <ypochta@yandex.ru>
2021-12-20 19:30:33 +0200

    Project Mesto rev.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,42 @@
-const formOpenButton = document.querySelector('.profile__edit-button');
-const formCloseButton = document.querySelector('.form__close');
-const form = document.querySelector('.form');
-const formSubmitButton = document.querySelector('.form__submit');
+const formOpenButton = document.querySelector('.profile__edit-button'); //Кнопка открытия формы редактирования
+const formCloseButton = document.querySelector('.popup__close');  // Кнопка закрытия формы
+const form = document.querySelector('.popup');  // Выбор формы
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
-let fieldName = document.querySelector('.form__field_el_name');
-let fieldDescription = document.querySelector('.form__field_el_description');
+let fieldName = document.querySelector('.popup__field_el_name');
+let fieldDescription = document.querySelector('.popup__field_el_description');
 
 function openForm(){
-  form.classList.add('form_opened');
+  form.classList.add('popup_opened');
   fieldName.value = profileTitle.textContent;
   fieldDescription.value = profileText.textContent;
 }
 
 function closeForm(){
-  form.classList.remove('form_opened');
+  form.classList.remove('popup_opened');
 }
 /*
 function toggleForm() {
   form.classList.toggle('form_opened');
 }
-*/
+
 function closeFormOnOverlayClick(event) {
   if (event.target === event.currentTarget){
-    form.classList.remove('form_opened');
+    form.classList.remove('popup_opened');
   }
-}
+}*/
 // Обработчик «отправки» формы
 function formSubmitHandler (evt){
   evt.preventDefault();
-  form.classList.remove('form_opened');
+  closeForm();
   profileTitle.textContent = fieldName.value;
   profileText.textContent = fieldDescription.value;
-
+  console.log('Событие отправки формы')
 }
 formOpenButton.addEventListener('click', openForm);
 formCloseButton.addEventListener('click', closeForm);
-formSubmitButton.addEventListener('click', formSubmitHandler);
-form.addEventListener('click', closeFormOnOverlayClick);
+form.addEventListener('submit', formSubmitHandler);
+//form.addEventListener('click', closeFormOnOverlayClick);
 
 
 
