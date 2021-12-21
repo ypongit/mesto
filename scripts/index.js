d046580b7a4a137d553286d872e8ab1bc7514a43
Yuri Ponomarev <ypochta@yandex.ru>
2021-12-21 10:51:51 +0200

    Project Mesto rev.3

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const formOpenButton = document.querySelector('.profile__edit-button'); //Кнопка открытия формы редактирования
 const formCloseButton = document.querySelector('.popup__close');  // Кнопка закрытия формы
 const form = document.querySelector('.popup');  // Выбор формы
+const mainForm = document.querySelector('.popup__main-container');
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
 let fieldName = document.querySelector('.popup__field_el_name');
@@ -31,11 +32,10 @@ function formSubmitHandler (evt){
   closeForm();
   profileTitle.textContent = fieldName.value;
   profileText.textContent = fieldDescription.value;
-  console.log('Событие отправки формы')
 }
 formOpenButton.addEventListener('click', openForm);
 formCloseButton.addEventListener('click', closeForm);
-form.addEventListener('submit', formSubmitHandler);
+mainForm.addEventListener('submit', formSubmitHandler);
 //form.addEventListener('click', closeFormOnOverlayClick);
 
 
