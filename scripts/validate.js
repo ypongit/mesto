98d4d34ad46fdf0142e78115500cf38f5e5d0b6d
Yuri Ponomarev <ypochta@yandex.ru>
2022-01-16 13:07:53 +0200

    Validation forms with separate file

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,31 +1,32 @@
 //<<<<<<<<<<<<<<<<<<<<< Валидация форм >>>>>>>>>>>>>>>>>>>>>>>>>>>>
-/*
-  mainEditForm - форма редактирования
-  inputProfileName - поле ввода формы редактирования
-  inputProfileDescription - поле описания формы редактирования
-  ------------------------------------------
-  mainAddForm - форма добавления
 
-*/
-/* const form = document.querySelector('.popup__main-container'); // выбор форм
-const formInput = form.querySelector('.popup__field');  // поля ввода форм
-const formError = form.querySelector(`.${formInput.id}-error`); // уникальное значение поля ввода */
+const validSettings = {
+  formSelector: '.popup__main-container',
+  inputSelector: '.popup__field',
+  submitButtonSelector: '.popup__submit',
+  inactiveButtonClass: 'popup__submit_inactive',
+  inputErrorClass: 'popup__field-error',
+  errorClass: 'popup__field_error_active'
+
+};
 
 // присвоение, сброс класса ошибки полю ввода
 const showInputError = (formElement, inputElement, errorMessage) => {
+  // console.log('inputElement =>', inputElement.id);
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  //inputElement.classList.add('popup__field_type_error');
   // Показываем сообщение об ошибке
   errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__field_error_active');
-}
+  errorElement.classList.add(validSettings.errorClass);
+};
+
 const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   //inputElement.classList.remove('popup__field_type_error');
   // Скрываем сообщение об ошибке
-  errorElement.classList.remove('popup__field_error_active');
+  errorElement.classList.remove(validSettings.errorClass);
   errorElement.textContent = '';
-}
+};
+
 // проверка корректности введенных данных
 const checkInputValidity = (formElement, inputElement) => {
   if (!inputElement.validity.valid){
@@ -33,11 +34,12 @@ const checkInputValidity = (formElement, inputElement) => {
   }else{
     hideInputError(formElement, inputElement);
   }
-}
+};
+
 // утановка слушателей на поля ввода формы
-const setEventListeners = (formElement) => {
-  const buttonElement = formElement.querySelector('.popup__submit');
-  const inputList = Array.from(formElement.querySelectorAll('.popup__field'));
+const setEventListeners = (formElement, submitButton, fieldInput) => {
+  const buttonElement = formElement.querySelector(submitButton);
+  const inputList = Array.from(formElement.querySelectorAll(fieldInput));
   toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function() {
@@ -46,24 +48,26 @@ const setEventListeners = (formElement) => {
     });
   });
 };
+
 // функция запуска проверки
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.popup__main-container'));
+const enableValidation = (forms) => {
+  console.log(forms);
+  const formList = Array.from(document.querySelectorAll(forms));
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', (evt) => {
     evt.preventDefault();
   });
-  setEventListeners(formElement);
+  setEventListeners(formElement, validSettings.submitButtonSelector, validSettings.inputSelector);
 });
 };
 
-// функция проверяет форму на наличие поля, непрошедшего проверку
+// проверяем форму на наличие поля, непрошедшего проверку
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
   })
 }
-
+// смена состояния кнопки активная|неактивная
 const toggleButtonState = (inputList, buttonElement) => {
   if(hasInvalidInput(inputList)){
     buttonElement.classList.add('popup__submit_inactive');
@@ -71,4 +75,5 @@ const toggleButtonState = (inputList, buttonElement) => {
     buttonElement.classList.remove('popup__submit_inactive');
   }
 };
-enableValidation();
+
+enableValidation(validSettings.formSelector);
